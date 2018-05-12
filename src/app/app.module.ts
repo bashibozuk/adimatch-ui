c86de96b985341fd7039246a5c3f91518cd399e2
Vasil Atanasov <vsatanasov@gmail.com>
2018-05-12 14:06:28 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HistoryComponent } from './history/history.component';
+import { EventsComponent } from './events/events.component';
+
+const appRoutes: Routes = [{
+  path: '', component: HomeComponent
+}, {
+  path: 'login', component: LoginComponent
+}, {
+  path: 'events', component: EventsComponent
+}, {
+  path: 'history', component: HistoryComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HistoryComponent,
+    EventsComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule
   ],
   providers: [],
